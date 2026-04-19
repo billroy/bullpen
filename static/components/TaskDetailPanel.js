6a095b80d6d4f6c5fb3c273c87e78cca718c7f40
Bill Roy <bill@Blackbird.local>
2026-04-19 15:12:38 -0400

    bullpen: Ticket detail: rename "Agent Output" to "Worker Output" [ticket-detail-rename-agent-output-to-worker-output-vRlQ]

--- static/components/TaskDetailPanel.js
+++ static/components/TaskDetailPanel.js
@@ -164,7 +164,7 @@ const TaskDetailPanel = {
 
         <div class="detail-output" v-if="agentOutput">
           <div class="detail-section-header">
-            <h3>Agent Output</h3>
+            <h3>Worker Output</h3>
             <button v-if="!readOnly" class="btn btn-sm btn-danger" @click="$emit('clear-output', task.id)">Clear</button>
           </div>
           <pre class="detail-output-content"><span v-for="line in parsedAgentOutputLines" :key="line.id" class="detail-output-line"><template v-if="line.commitHash">{{ line.prefix }}<button class="detail-output-commit" @click="openCommitDiff(line.commitHash)">{{ line.commitHash }}</button>{{ line.suffix }}</template><template v-else>{{ line.prefix }}</template></span></pre>
